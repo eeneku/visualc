921f983a3a94811e028272211b245fbf944594b0
eeneku <eeneku@gmail.com>
2014-09-11 10:35:24 +0300

    Päivitetty harjoitusta 11

--- Harjoitus11/main.c
+++ Harjoitus11/main.c
@@ -2,41 +2,50 @@
 
 int main()
 {
-	int kuukausi = 0;
+	char merkki;
 
 	do
 	{
-		printf("Anna kuukausi (luku 1-12).\n");
-		scanf_s("%d", &kuukausi);
-	} while (kuukausi < 1 || kuukausi > 12);
+		int kuukausi = 0;
+		
+
+		do
+		{
+			printf("Anna kuukausi (luku 1-12).\n");
+			scanf_s("%d", &kuukausi);
+		} while (kuukausi < 1 || kuukausi > 12);
 
 
-	switch (kuukausi)
-	{
-	case 1:
-	case 3:
-	case 5:
-	case 7:
-	case 8:
-	case 10:
-	case 12:
-		printf("Kuukaudessa on 31 paivaa.");
-		break;
-	case 4:
-	case 6:
-	case 9:
-	case 11:
-		printf("Kuukaudessa on 30 paivaa.");
-		break;
-	case 2:
-		printf("Kuukaudessa on 28 paivaa (karkausvuonna 29).");
-		break;
-	default:
-		printf("Oho..");
-		break;
-	}
+		switch (kuukausi)
+		{
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			printf("Kuukaudessa on 31 paivaa.");
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			printf("Kuukaudessa on 30 paivaa.");
+			break;
+		case 2:
+			printf("Kuukaudessa on 28 paivaa (karkausvuonna 29).");
+			break;
+		default:
+			printf("Oho..");
+			break;
+		}
 
-	printf("\n");
+		printf("\n\nHaluatko jatkaa (k/e)?\n");
+		fflush(stdin);
+		scanf_s("%c", &merkki, 1);
+
+	} while (merkki == 'k');
 
 	return 0;
 }
