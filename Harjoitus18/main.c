b8f3f71b577bc8df7e7063ee52f30d19fe97ac1c
eeneku <eeneku@gmail.com>
2014-09-17 14:59:32 +0300

    Harjoitus18:n lähdekoodi...

---
+++ Harjoitus18/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+	int kertoja = 0, kertotaulu = 1, syote = 0, oikein = 0;
+
+	printf("Anna kertoja, niin kyselen sen kertotaulun. He.\n");
+	scanf_s("%d", &kertoja);
+
+	while (kertotaulu <= 10)
+	{
+		printf("Paljonko on %d * %d? ", kertoja, kertotaulu);
+		scanf_s("%d", &syote);
+
+		if (syote == kertoja*kertotaulu) oikein++;
+
+		kertotaulu++;
+	}
+
+	printf("\nSait %d/10!\n\n", oikein);
+
+	if (oikein < 4) printf("Noo ois se paremminkin voinu menna..\n");
+	else if (oikein > 7) printf("No sehan meni hyvin!\n");
+	else printf("Ihan hyva suoritus.\n");
+
+	return 0;
+}
